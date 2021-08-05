4709de180ce355b66f872bdaca3cba796fb72e23
Matthew Fritsch <matthewmfritsch@gmail.com>
2021-08-05 09:14:59 -1000

    Added logger to Console class

--- src/Console.hpp
+++ src/Console.hpp
@@ -2,7 +2,7 @@
 #ifndef CONSOLE_HPP
 #define CONSOLE_HPP
 
-#include <iostream>
+#include "Utils.hpp"
 
 #define U8BI_MAX 0b11111111
 
@@ -15,18 +15,20 @@ struct Coord{
 class Console
 {
     public:
-        Console();
+        Console(Logger *lgr = nullptr);
         ~Console();
         char GetItemAt(Coord c);
         void Update();
-        Coord GetTerminalSize();
+        Coord GetTerminalSize(uint8_t &wid, uint8_t &hei);
         bool PlaceAt(char c, uint8_t x, uint8_t y);
 
     private:
-        bool InBounds(Coord c);            
+        bool InBounds(Coord c);
+        void Log(std::string);
+
         uint16_t frame_ctr = 0;
         std::string *board = nullptr;
-
+        Logger *logger;
         uint8_t term_width_;
         uint8_t term_height_;
 };
