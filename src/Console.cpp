1e690a2b0d5fb8b2b90d6b7865d0b085a5219af8
Matthew Fritsch <matthewmfritsch@gmail.com>
2021-08-05 09:14:01 -1000

    Console now uses logger, and prints based on the terminal size

--- src/Console.cpp
+++ src/Console.cpp
@@ -3,12 +3,10 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-Console::Console()
+Console::Console(Logger *lgr = nullptr) : logger(lgr)
 {
-    Coord max_term = GetTerminalSize();
-    term_width_ = max_term.x;
-    term_height_= max_term.y;
-    std::cout << "Found console with dimensions " << (int)term_width_ << "x" << (int)term_height_ << std::endl;
+    Coord max_term = GetTerminalSize(term_width_, term_height_);
+    Log("Found console with dimensions " + std::to_string(term_width_) + "x" + std::to_string(term_height_));
     
     board = new std::string[term_height_];
 }
@@ -18,40 +16,42 @@ Console::~Console()
     delete[] board;
 }
 
-Coord Console::GetTerminalSize()
+Coord Console::GetTerminalSize(uint8_t &wid, uint8_t &hei)
 {
     struct winsize wsz;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &wsz);
+    wid = wsz.ws_col;
+    hei = wsz.ws_row;
     Coord c(wsz.ws_col, wsz.ws_row);
     return c;
 }
 
 bool Console::PlaceAt(char c, uint8_t x, uint8_t y)
 {
-    std::cout << "PlaceAt" << std::endl;
-    
+    Log("PlaceAt");
     return false;
 }
 
 void Console::Update()
 {
-        frame_ctr++;
-        for(uint8_t line_num = 0; line_num < term_height_; line_num++)
+    GetTerminalSize(term_width_, term_height_);
+    frame_ctr++;
+    for(uint8_t line_num = 0; line_num < term_height_; line_num++)
+    {
+        std::cout << std::endl;
+        for(uint8_t x = 0; x < term_width_; x++)
         {
-            std::cout << std::endl;
-            for(uint8_t x = 0; x < term_width_; x++)
+            if(x == term_width_-1 && line_num == term_height_-1)
             {
-                if(x == term_width_-1 && line_num == term_height_-1)
-                {
-                    std::cout.flush();
-                    std::cout << "X";
-                }
-                else if(x == 0 || line_num == 0 || x == term_width_-1 || line_num == term_height_-1)
-                    std::cout << "X";
-                else
-                    std::cout << " ";
+                std::cout.flush();
+                std::cout << "X";
             }
+            else if(x == 0 || line_num == 0 || x == term_width_-1 || line_num == term_height_-1)
+                std::cout << "X";
+            else
+                std::cout << " ";
         }
+    }
 }
 
 char Console::GetItemAt(Coord c)
@@ -63,4 +63,10 @@ char Console::GetItemAt(Coord c)
 bool Console::InBounds(Coord c)
 {
     return c.x > 0 && c.x < term_width_ && c.y > 0 && c.y < term_height_;
+}
+
+void Console::Log(std::string toPrint)
+{
+    if(!logger) return;
+    logger->Log(toPrint);
 }
